442008d032973c6f44a2739c804782d798b20d70
KirX84 <KirX84@gmail.com>
2022-06-23 16:24:23 +0300

    Add commands

--- main.cpp
+++ main.cpp
@@ -10,11 +10,45 @@
 /***************************************************************************************/
 //typedef enum {FALSE = 0, TRUE = !FALSE} bool;
 //int i = 0;
-unsigned char Lazer_mm[8] = "\xFA\x04\x0C\x02\xF4",
-              Lazer_0c[8] = "\xFA\x04\x05\x00\xFD",  //Временной интервал (0 с)
-              Lazer_1g[8] = "\xFA\x04\x0A\x00\xF8",
-              Lazer_start[7] = "\x80\x06\x03\x77",
-              testBuffer[6] = "\xFA\x04\x0C\x02\xF4",
+unsigned char //Установить разрешение:
+              Lazer_1mm[8] = "\xFA\x04\x0C\x01\xF5",		  //Разрешение 1мм
+              Lazer_01mm[8] = "\xFA\x04\x0C\x02\xF4",		  //Разрешение 0.1мм
+			  //Установить временной интервал:
+			  Lazer_1c[8]=  "\xFA\x04\x05\x01\xFC",			  //1 с
+              Lazer_0c[8] = "\xFA\x04\x05\x00\xFD",			  //0 с
+			  //Установить частоту:
+              Lazer_1g[8] = "\xFA\x04\x0A\x00\xF8",			  //Установить частоту 1Гц
+			  Lazer_5g[8] = "\xFA\x04\x0A\x05\xF3",			  //Установить частоту 5Гц
+			  Lazer_10g[8] = "\xFA\x04\x0A\x0A\xEE",		  //Установить частоту 10Гц
+			  Lazer_20g[8] = "\xFA\x04\x0A\x14\xE4",		  //Установить частоту 20Гц
+			  //Установить режим измерения
+			  Lazer_one_zam[7] "\x80\x06\x02\x78"			  //Один замер
+              Lazer_nepr_zam[7] = "\x80\x06\x03\x77",		  //Непрерывное измерение
+
+			  Lazer_shtd[7] = "\x80\x04\x02\x7A",             //Отключение устройства
+			  Lazer_adress[8] = "\xFA\x04\x01\x80\x81",       //Установить адрес
+			  //Установите начало измерения при включении питания:
+			  Lazer_start_off[8] = "\xFA\x04\x0D\x00\xF5",	 //Выключить
+			  Lazer_start_on[8] = "\xFA\x04\x0D\x01\xF4",	 //Включить
+			  //Установить диапазон:
+			  Lazer_5m[8] = "\xFA\x04\x09\x05\xF4",		     //5 м
+			  Lazer_10m[8] = "\xFA\x04\x09\x0a\xEF",		 //10 м
+			  Lazer_30m[8] = "\xFA\x04\x09\x1E\xDB",		 //30 м
+			  Lazer_50m[8] = "\xFA\x04\x09\x32\xC7",		 //50 м
+			  Lazer_80m[8] = "\xFA\x04\x09\x50\xA9",		 //80 м
+
+			  Lazer_dal_min[9] = "\xFA\x04\x06\x2D\x01\xCE",   //Изменение расстояния-1
+			  Lazer_dal_pl[9] = "\xFA\x04\x06\x2B\x\x01\xD0",  //Изменение расстояния+1
+			  //Установить начальную точку
+			  Lazer_top[8] = "\xFA\x04\x08\x01\xF9",        //верх
+			  Lazer_back[8] = "\xFA\x04\x08\x00\xFA",       //низ
+			  
+			  Единое измерение (трансляция)	$FA$06$06$FA
+			  Чтение кэша:			$80$06$07$73	80 06 82 30 30 33 2E 35 36 39 35 5E
+			  Контрольный лазер:
+			  $80$06$05$01$74	Открытый	80 06 85 01 F4
+			  $80$06$05$00$75	Закрыть		80 06 85 01 F4
+
 			  Otvet_Lazer[12],
 			  Otvet_Lazer_TXT[9],
 			  i, incomingByte, idx, j, Error_j;
@@ -86,7 +120,7 @@ int main(void)
 	UART_SendData(Lazer_1g,5);
 	while(!readyToExchange);
 	//_delay_ms(10);
-	UART_SendData(Lazer_start,4);
+	UART_SendData(Lazer_nepr_zam,4);
 	while(!readyToExchange);
 	//_delay_ms(10);
 	/*memcpy(testBuffer, 

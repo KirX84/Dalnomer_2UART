3ac29534096758d39da887eabdca26f97ec5e535
KirX84 <KirX84@gmail.com>
2022-08-09 14:19:51 +0300

    Revert "Revert "fixing vector_circ""
    This reverts commit 7a2d010ce60412686bf1755b94a892f9053f14c2.

--- main.cpp
+++ main.cpp
@@ -12,34 +12,40 @@
 //typedef enum {FALSE = 0, TRUE = !FALSE} bool;
 //int i = 0;
 template <typename T>
+struct Node
+{
+	T val;
+	Node* next;
+	Node(T _val) : val(_val), next(NULL){}
+};
 class vector_circ
 {
 	public:
 		T val;
 		int size;
-		T* begin, curr, next;
+		Node* first, curr;
 		vector_circ()
 		{	
-		    begin = new(T);
-			curr = begin;	
-			for(int i = 0; i < 31; i++)
+		    first = new(T);
+			curr = first;	
+			for(int i = 0; i < size; i++)
 			{
 				curr->next = new(T);
 				curr = curr->next;
 			}
-			curr->next = begin;
-			curr = begin;
+			curr->next = first;
+			curr = first;
 		}
-		~vector_circ() //переписать
+		~vector_circ()
 		{
 			while(!curr->next)
 			{
-				curr = begin->next;
-				delete(begin);
-				begin = curr;
+				curr = first->next;
+				delete(first);
+				first = curr;
 			}
-			delete(begin);
-
+			curr = NULL;
+			delete(first);			
 		}
 		
 		void push_back( const T& value)
